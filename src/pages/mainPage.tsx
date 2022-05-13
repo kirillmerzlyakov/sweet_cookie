5d1cc0690e40ec10560fc6d87cf9c05fed9b021d
kirillmerzlyakov <merslyakovkg6@yandex.ru>
2022-05-14 01:33:10 +0500

    сделал страницу тарифов

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,34 +1,38 @@
 import React from "react";
 import { triangle, voiciaTitle } from "../media/mediaSVG";
 import { BlackBlock } from "./blackBlock";
-import cn from "./mainPage.module.less";
+import s from "./mainPage.module.less";
+import cn from "classnames"
+import { TariffBlock } from "./tariffBlock";
 
 export const MainPage: React.FC = () => (
-  <div className={cn.mainPage}>
-    <div className={cn.firstBlock}>
-      <div className={cn.titleWrapper}>
-        <div className={cn.left}>
-          <div className={cn.title}>{voiciaTitle()}</div>
-          <div className={cn.subtitle}>интеллектуальный голосовой робот</div>
-          <div className={cn.description1}>
+  <div className={s.mainPage}>
+    <div className={s.firstBlock}>
+      <div className={s.titleWrapper}>
+        <div className={s.left}>
+          <div className={s.title}>{voiciaTitle()}</div>
+          <div className={s.subtitle}>интеллектуальный голосовой робот</div>
+          <div className={s.description1}>
             интеллектуальный голосовой робот, который работает с клиентами от
             первого звонка до получения денег
           </div>
-          <div className={cn.buttons}>
-            <div className={cn.button1}>собрать робота</div>
-            <div className={cn.button2}>
+          <div className={s.buttons}>
+            <div className={s.button}>собрать робота</div>
+            <div className={cn(s.button, s.button2)}>
               {triangle()}&nbsp; познакомиться с voicia
             </div>
           </div>
         </div>
-        <div className={cn.right}>
+        <div className={s.right}>
           снизьте издержки на поддержку клиентов и продаже до{" "}
-          <span className={cn.discont}>-50%</span>
+          <span className={s.discont}>-50%</span>
         </div>
       </div>
     </div>
-    <div className={cn.secondBlock}></div>
-    <div className={cn.thirdBlock}></div>
+    <div className={s.secondBlock}></div>
+    <div className={s.thirdBlock}></div>
     <BlackBlock />
+    <TariffBlock />
+
   </div>
 );
---
+++ src/pages/tariffBlock.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import s from "./tariffBlock.module.less";
+import s2 from "./mainPage.module.less";
+import cn from "classnames";
+
+export const TariffBlock: React.FC = () => (
+  <div className={s.tariffBlock}>
+    <div className={s.title}>тарифы voicia</div>
+    <div className={s.subtitle}>
+      На запуск Voicia под ваши задачи нам понадобится <br />
+      от 3-х рабочих дней, а вам - уделить нам от 3-х часов своего времени
+    </div>
+    <div className={s.wrapperText}>
+      <div className={s.title1}>готовое решение</div>
+
+      <div className={s.featureWrapper}>
+        <div className={s.featureTitle}>быстрый запуск робота в работу</div>
+        <div className={s.featuredescription}>3 – 5 дней</div>
+      </div>
+
+      <div className={s.featureWrapper}>
+        <div className={s.featureTitle}>базовые интенты</div>
+        <div className={s.featuredescription}>собраны под данное решение</div>
+      </div>
+
+      <div className={s.featureWrapper}>
+        <div className={s.featureTitle}>структура робота</div>
+        <div className={s.featuredescription}>
+          протестирована и ее нельзя редактировать
+        </div>
+      </div>
+
+      <div className={s.featureWrapper}>
+        <div className={s.featureTitle}>специфика клиента</div>
+        <div className={s.featuredescription}>
+          робот адаптируется под ваш бизнес
+        </div>
+      </div>
+
+      <div className={s.featureWrapper}>
+        <div className={s.featureTitle}>интеграция</div>
+        <div className={s.featuredescription}>с CRM-системой</div>
+      </div>
+    </div>
+
+    <div className={cn(s2.button, s.button1)}>заказать готовое решение</div>
+  </div>
+);
