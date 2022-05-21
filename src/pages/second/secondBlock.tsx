70121ee20ee8ebb535e9704cc763dfecf85d3e2b
kirillmerzlyakov <merslyakovkg6@yandex.ru>
2022-05-21 16:33:02 +0500

    фиксы

--- src/pages/second/secondBlock.tsx
+++ src/pages/second/secondBlock.tsx
@@ -3,7 +3,7 @@ import s from "./secondBlock.module.less";
 import cn from "classnames";
 import Img1 from "../../media/man1.png";
 import Img2 from "../../media/man2.png";
-import { arrowDown, triangleBig } from "../../media/mediaSVG";
+import { arrowDown, check, triangleBig } from "../../media/mediaSVG";
 import { Slider } from "../../components/slider/Slider";
 
 export const SECOND_BLOCK_ID = "second-block-id";
@@ -20,12 +20,16 @@ export const SecondBlock: React.FC = () => (
           на робота VOICIA
         </div>
         <div className={s.itemsWrapper}>
-          {renderSmallItem("600", "звонков/день 3 менеджера")}
-          {renderSmallItem("22%", "процент конверсии")}
-          {renderSmallItem("32₽", "цена одного участника вебинара")}
-          {renderItem("6000", "звонков за полчаса")}
-          {renderItem("19%", "процент конверсии")}
-          {renderItem("19₽", "цена одного участника вебинара")}
+          <div className={s.row}>
+            {renderSmallItem("600", "звонков/день 3 менеджера")}
+            {renderSmallItem("22%", "процент конверсии")}
+            {renderSmallItem("32₽", "цена одного участника вебинара")}
+          </div>
+          <div className={s.row}>
+            {renderItem("6000", "звонков за полчаса")}
+            {renderItem("19%", "процент конверсии")}
+            {renderItem("19₽", "цена одного участника вебинара")}
+          </div>
         </div>
 
         <div className={s.play}>{triangleBig()}</div>
@@ -42,11 +46,15 @@ export const SecondBlock: React.FC = () => (
           <img src={Img2} alt="man1"></img>
         </div>
 
-        <div className={s.itemsWrapper} style={{ width: "300px" }}>
-          {renderSmallItem("5000", "звонков/день 25 менеджеров")}
-          {renderSmallItem("410₽", "цена заявки")}
-          {renderItem("30 000", "звонков/день")}
-          {renderItem("180₽", "цена заявки")}
+        <div className={s.itemsWrapper}>
+          <div className={s.row}>
+            {renderSmallItem("5000", "звонков/день 25 менеджеров")}
+            {renderSmallItem("410₽", "цена заявки")}
+          </div>
+          <div className={s.row}>
+            {renderItem("30 000", "звонков/день")}
+            {renderItem("180₽", "цена заявки")}
+          </div>
         </div>
         <div className={cn(s.play)}>{triangleBig()}</div>
       </div>
@@ -62,11 +70,27 @@ export const SecondBlock: React.FC = () => (
         <div className={s.img2}>
           <img src={Img1} alt="man1"></img>
         </div>
-        <div className={s.itemsWrapper} style={{ width: "300px" }}>
-          {renderSmallItem("8%", "конверсия в соискателя на поиске")}
-          {renderSmallItem("2000", "звонков/день 10 менеджеров")}
-          {renderItem("10%", "конверсия в соискателя на поиске")}
-          {renderItem("19%", "процент конверсии")}
+        <div className={s.itemsWrapper}>
+          <div className={s.row}>
+            {renderSmallItem("8%", "конверсия в соискателя на поиске")}
+            {renderSmallItem("2000", "звонков/день 10 менеджеров")}
+          </div>
+          <div className={s.row}>
+            {renderItem("10%", "конверсия в соискателя на поиске")}
+            <div className={s.checkTextWrapper}>
+              <div className={s.checkItem}>
+                <div className={s.check}>{check()}</div>
+                <div>масштабировали исходящие звонки</div>
+              </div>
+              <div className={s.checkItem}>
+                <div className={s.check}>{check()}</div>
+                <div>
+                  автоматическая квалификация соискателя по типу курьер на
+                  велосипеде или курьер на авто
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className={cn(s.play)}>{triangleBig()}</div>
       </div>
@@ -80,12 +104,16 @@ export const SecondBlock: React.FC = () => (
           на робота VOICIA
         </div>
         <div className={s.itemsWrapper}>
-          {renderSmallItem("600", "звонков/день 3 менеджера")}
-          {renderSmallItem("22%", "процент конверсии")}
-          {renderSmallItem("32₽", "цена одного участника вебинара")}
-          {renderItem("6000", "звонков за полчаса")}
-          {renderItem("19%", "процент конверсии")}
-          {renderItem("19₽", "цена одного участника вебинара")}
+          <div className={s.row}>
+            {renderSmallItem("600", "звонков/день 3 менеджера")}
+            {renderSmallItem("22%", "процент конверсии")}
+            {renderSmallItem("32₽", "цена одного участника вебинара")}
+          </div>
+          <div className={s.row}>
+            {renderItem("6000", "звонков за полчаса")}
+            {renderItem("19%", "процент конверсии")}
+            {renderItem("19₽", "цена одного участника вебинара")}
+          </div>
         </div>
         <div className={cn(s.play, s.playWhite)}>{triangleBig(true)}</div>
       </div>
