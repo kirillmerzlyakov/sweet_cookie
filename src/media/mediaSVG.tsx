fa79c11e3809f39ca7ffc7cc6f3c776207576f47
kirillmerzlyakov <merslyakovkg6@yandex.ru>
2022-05-28 19:33:46 +0500

    fix svg

--- src/media/mediaSVG.tsx
+++ src/media/mediaSVG.tsx
@@ -94,8 +94,6 @@ export const triangleBig = (isWhite?: boolean) => (
 
 export const triangleMedium = () => (
   <svg
-    width="23"
-    height="26"
     viewBox="0 0 23 26"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
