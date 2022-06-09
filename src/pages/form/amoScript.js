d94e9e2125d96aee4e74c47c19b7d3ce0d812506
kirillmerzlyakov <merslyakovkg6@yandex.ru>
2022-06-09 23:47:08 +0500

    добавил форму amocrm

---
+++ src/pages/form/amoScript.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+!(function (a, m, o, c, r) {
+  (a[o + c] = a[o + c] || {
+    setMeta: function (p) {
+      this.params = (this.params || []).concat([p]);
+    },
+  }),
+    (a[o + r] =
+      a[o + r] ||
+      function (f) {
+        a[o + r].f = (a[o + r].f || []).concat([f]);
+      }),
+    a[o + r]({
+      id: "942334",
+      hash: "6b68fa8c8e8dafe51894896e7f699842",
+      locale: "ru",
+    }),
+    (a[o + m] =
+      a[o + m] ||
+      function (f, k) {
+        a[o + m].f = (a[o + m].f || []).concat([[f, k]]);
+      });
+})(window, 0, "amo_forms_", "params", "load", "loaded");
--- src/pages/form/formBlock.tsx
+++ src/pages/form/formBlock.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import cn from "classnames";
-import s1 from "../mainPage.module.less";
 import s from "./formBlock.module.less";
-import axios from "axios";
 
 export const FORM_BLOCK_ID = "form-block-id";
 
 export const FormBlock: React.FC = () => {
-  const [name, setName] = React.useState("");
-  const [phone, setPhone] = React.useState("");
-  const [text, setText] = React.useState("");
-  const [check1, setCheck1] = React.useState(true);
-  const [check2, setCheck2] = React.useState(true);
-
-  const disabledButton = !(check1 && check2);
+  const myRef = React.useRef<HTMLDivElement>(null);
+  React.useEffect(() => {
+    const docCreateElem = document.createElement("script");
+    docCreateElem.src =
+      "https://forms.amocrm.ru/forms/assets/js/amoforms.js?1654540677";
+    docCreateElem.id = "amoforms_script_942334";
+    myRef.current!.appendChild(docCreateElem);
+    require("./amoScript");
+  }, []);
 
   return (
     <div className={s.tariffBlock} id={FORM_BLOCK_ID}>
@@ -28,86 +27,8 @@ export const FormBlock: React.FC = () => {
         </div>
         <></>
 
-        <form action="http://localhost:14444/" method="POST">
-          <div className={s.formWrapper}>
-            <input
-              className={s.input}
-              placeholder="имя"
-              name="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-            <input
-              className={s.input}
-              placeholder="телефон"
-              name="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            ></input>
-            <textarea
-              className={cn(s.input, s.textarea)}
-              name="text"
-              value={text}
-              placeholder="кратко расскажите о вашем бизнесе"
-              onChange={(e) => setText(e.target.value)}
-            />
-          </div>
-
-          <div className={s.checkboxWrapper}>
-            <div className={s.checkboxRow}>
-              <input
-                className={s.checkbox}
-                placeholder="имя"
-                type={"checkbox"}
-                checked={check1}
-                name="check1"
-                onChange={(e) => setCheck1(!check1)}
-              />
-              <div className={s.checkboxDescription}>
-                Да, я хочу получать новостную рассылку о Voicia
-              </div>
-            </div>
-            <div className={s.checkboxRow}>
-              <input
-                className={s.checkbox}
-                placeholder="имя"
-                type={"checkbox"}
-                name="check2"
-                checked={check2}
-                onChange={(e) => setCheck2(!check2)}
-              ></input>
-              <div className={s.checkboxDescription}>
-                Даю согласие на{" "}
-                <span className={s.boldText}>
-                  обработку персональных данных
-                </span>
-              </div>
-            </div>
-          </div>
-
-          <div className={s.buttons}>
-            <input
-              type="submit"
-              value="отправить"
-              className={cn(
-                s1.button,
-                s.button,
-                disabledButton && s.buttonDisabled
-              )}
-              disabled={disabledButton}
-            />
-          </div>
-        </form>
+        <div ref={myRef} id={"amoForm"} className={s.amoForm} />
       </div>
-
-      {/* <iframe src="https://forms.amocrm.ru/forms/assets/js/amoforms.js?1654513347"></iframe> */}
-
-      {/* <div></div> */}
     </div>
   );
 };
-
-// axios
-// .get("https://merslyakovkg6.amocrm.ru/api/v2/leads")
-// .then((response) => console.log(response))
-// .catch((err) => console.log(err));
