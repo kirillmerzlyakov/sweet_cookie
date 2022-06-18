e9d4e200d5c7711292c84221a08772e987e7bbde
kirillmerzlyakov <merslyakovkg6@yandex.ru>
2022-06-19 00:58:16 +0500

    добавил аудио на главную

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { triangle, voiciaLogo } from "../media/mediaSVG";
 import { BlackBlock } from "./black/blackBlock";
 import s from "./mainPage.module.less";
@@ -12,50 +12,63 @@ import { Footer } from "../components/footer/footer";
 import { scrollTo } from "../components/shared";
 import { Slider } from "../components/slider/Slider";
 import Marquee from "react-fast-marquee";
+import AudioSrc from "./../media/audio/main.mp3";
 
-export const MainPage: React.FC = () => (
-  <div className={s.mainPage}>
-    <div className={s.firstBlock}>
-      <div className={s.circle} />
-      <div className={s.circle} />
-      <div className={s.circle} />
-      <div className={s.titleWrapper}>
-        <div className={s.left}>
-          <div className={s.title}>{voiciaLogo()}</div>
-          <div className={s.subtitle}>интеллектуальный голосовой робот</div>
-          <div className={s.description1}>
-            интеллектуальный голосовой робот, который работает с клиентами от
-            первого звонка до получения денег
+export const MainPage: React.FC = () => {
+  const audio = useMemo(() => new Audio(AudioSrc), []);
+  const [play, setPlay] = useState(false);
+
+  React.useEffect(() => {}, []);
+  return (
+    <div className={s.mainPage}>
+      <div className={s.firstBlock}>
+        <div className={s.circle} />
+        <div className={s.circle} />
+        <div className={s.circle} />
+        <div className={s.titleWrapper}>
+          <div className={s.left}>
+            <div className={s.title}>{voiciaLogo()}</div>
+            <div className={s.subtitle}>интеллектуальный голосовой робот</div>
+            <div className={s.description1}>
+              интеллектуальный голосовой робот, который работает с клиентами от
+              первого звонка до получения денег
+            </div>
+            <div className={s.buttons}>
+              <div className={s.button} onClick={() => scrollTo(FORM_BLOCK_ID)}>
+                собрать робота
+              </div>
+              <div
+                className={cn(s.button, s.buttonWhite)}
+                onClick={() => {
+                  play ? audio.pause() : audio.play();
+                  setPlay(!play);
+                }}
+              >
+                {triangle(play ? "#b0d1fb": undefined)}&nbsp; познакомиться с voicia
+              </div>
+            </div>
           </div>
-          <div className={s.buttons}>
-            <div className={s.button} onClick={() => scrollTo(FORM_BLOCK_ID)}>
-              собрать робота
-            </div>
-            <div className={cn(s.button, s.buttonWhite)}>
-              {triangle()}&nbsp; познакомиться с voicia
-            </div>
+          <div className={s.right}>
+            снизьте издержки на поддержку клиентов и продаже до{" "}
+            <span className={s.discont}>-50%</span>
           </div>
         </div>
-        <div className={s.right}>
-          снизьте издержки на поддержку клиентов и продаже до{" "}
-          <span className={s.discont}>-50%</span>
+        <div className={s.bgImg} />
+        <div className={s.marquee}>
+          <Marquee speed={70} gradient={false}>
+            снизьте издержки на поддержку клиентов и продаже до -50%.&nbsp;
+            снизьте издержки на поддержку клиентов и продаже до -50%.&nbsp;
+          </Marquee>
         </div>
       </div>
-      <div className={s.bgImg} />
-      <div className={s.marquee}>
-        <Marquee speed={70} gradient={false}>
-          снизьте издержки на поддержку клиентов и продаже до -50%.&nbsp;
-          снизьте издержки на поддержку клиентов и продаже до -50%.&nbsp;
-        </Marquee>
-      </div>
+      <SecondBlock />
+      <Slider />
+      <BlackBlock />
+      <StepsBlock />
+      <TariffBlock />
+      <CompaniesBlock />
+      <FormBlock />
+      <Footer />
     </div>
-    <SecondBlock />
-    <Slider />
-    <BlackBlock />
-    <StepsBlock />
-    <TariffBlock />
-    <CompaniesBlock />
-    <FormBlock />
-    <Footer />
-  </div>
-);
+  );
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,8 @@
 /// <reference types="react-scripts" />
 declare module "react-slideshow-image";
 declare module '*.less';
+
+declare module '*.mp3' {
+    const src: string;
+    export default src;
+  }
